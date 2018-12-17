d565e78322e056aa3c0f794904a4b43fc72bb6b9
mehdiraddadi <mehdi.raddadi@gmail.com>
2018-12-17 13:51:06 +0100

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-let products = require('./products.json')
+var fs = require('fs');
 
-console.log(products)
+fs.readFile('./mock/products.json', 'utf8', function (err, data) {
+    if (err) throw err; // we'll not consider error handling for now
+    var obj = JSON.parse(data);
+    console.log(obj)
+});
+
+
